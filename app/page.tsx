3f69de959c49ef126ec936518f2e4e1b9c652e34
Anthony Bui <anthonybui@gmail.com>
2025-04-13 11:31:21 -0700

    support missing types... including a quick mention implementation

--- app/page.tsx
+++ app/page.tsx
@@ -8,24 +8,52 @@ interface Node {
   children?: Node[]
 }
 
+interface MentionNode extends Node {
+  type: 'mention'
+  color: string
+  id: string
+  title: string
+  value: string
+  variableType: string
+}
+
 const renderNode = (node: Node): JSX.Element | null => {
-  console.log(node)
   if (node.text && node.type === undefined) {
     return <Text text={node.text} bold={!!node.bold} />;
   }
 
   switch (node.type) {
     case 'block':
-      console.log("TODO: Implement 'title")
-      return <div className="bg-indigo-500">{node.children?.map((child: any, idx: number) => renderNode(child))}</div>;
+      return <div className="">{node.children?.map((child: any, idx: number) => renderNode(child))}</div>;
     case 'p':
-      return <p className="bg-green-500">{node.children?.map((child: any, idx: number) => renderNode(child))}</p>;
+      return <div className="">{node.children?.map((child: any, idx: number) => renderNode(child))}</div>;
     case 'h1':
-      return <h1 className="border">{node.children?.map((child: any, idx: number) => renderNode(child))}</h1>;
+      return <h1 className="">{node.children?.map((child: any, idx: number) => renderNode(child))}</h1>;
+    case 'h2':
+      return <h2 className="">{node.children?.map((child: any, idx: number) => renderNode(child))}</h2>;
+    case 'h3':
+      return <h3 className="">{node.children?.map((child: any, idx: number) => renderNode(child))}</h3>;
+    case 'h4':
+      return <h4 className="border">{node.children?.map((child: any, idx: number) => renderNode(child))}</h4>;
+    case 'h5':
+      return <h5 className="border">{node.children?.map((child: any, idx: number) => renderNode(child))}</h5>;
+    case 'h6':
+      return <h6 className="border">{node.children?.map((child: any, idx: number) => renderNode(child))}</h6>;
+    case 'clause':
+      return <div className="">{node.title} {node.children?.map((child: any, idx: number) => renderNode(child))}</div>;
+    case 'ul':
+      return <ul className="">{node.children?.map((child: any, idx: number) => renderNode(child))}</ul>;
+    case 'li':
+      return <li className="">{node.children?.map((child: any, idx: number) => renderNode(child))}</li>;
+    case 'lic':
+      return <li className="">{node.children?.map((child: any, idx: number) => renderNode(child))}</li>;
+    case 'mention':
+      const mentionNode = node as MentionNode;
+      return <div className="inline-block" style={{ backgroundColor: mentionNode.color }}>{mentionNode.value}</div>;
+    default: 
+      console.log("Unknown type: ", node.type, node)
+      return null;
   }
-
-
-  return null
 };
 
 function Text({ text, bold }: { text: string, bold?: boolean }) {
