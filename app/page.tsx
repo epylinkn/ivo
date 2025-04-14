15ab481f89acdbf3e5619a7dfef097b9636551fc
Anthony Bui <anthonybui@gmail.com>
2025-04-14 12:06:02 -0700

    :nail_care:

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import { TreeRenderer } from "@/components/tree-renderer";
+import { TreeRenderer } from '@/components/tree-renderer'
 
 export default function Home() {
-  const tree = require("../data/input.json");
+  const tree = require('../data/input.json')
 
   return (
-    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="text-foreground dark:text-background">
+    <div className='min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <main className='text-foreground dark:text-background'>
         <TreeRenderer node={tree[0]} />
       </main>
     </div>
-  );
+  )
 }
--- components/mention.tsx
+++ components/mention.tsx
@@ -1,14 +1,20 @@
 'use client'
 
 interface MentionProps {
-  value: string;
-  color: string;
-  id: string;
-  title: string;
-  variableType: string;
+  value: string
+  color: string
+  id: string
+  title: string
+  variableType: string
 }
 
-export default function Mention({ value, color, id, title, variableType }: MentionProps) {
+export default function Mention({
+  value,
+  color,
+  id,
+  title,
+  variableType,
+}: MentionProps) {
   return (
     <span
       data-id={id}
@@ -17,9 +23,10 @@ export default function Mention({ value, color, id, title, variableType }: Menti
       onClick={() => {
         alert('TODO: change color')
       }}
-      className="inline-block text-white px-2 py-1 rounded"
-      style={{ backgroundColor: color }}>
+      className='inline-block text-white px-2 py-1 rounded'
+      style={{ backgroundColor: color }}
+    >
       {value}
     </span>
-  );
+  )
 }
